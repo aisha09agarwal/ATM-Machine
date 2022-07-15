866414998066d8e95519995bb69a1c890b26bb76
Aisha Agarwal <78947668+aisha09agarwal@users.noreply.github.com>
2022-07-15 16:44:06 +0530

    Add files via upload

---
+++ Atm_machine/ATM.java
@@ -0,0 +1,12 @@
+package Projects.Atm_machine;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu{
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		
+		optionMenu.getLogin();
+	}
+
+}
